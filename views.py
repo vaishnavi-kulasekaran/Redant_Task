031bc3a0b91e1f9d503cd38f86593a69a39e493c
vaishnavi-kulasekaran <72563225+vaishnavi-kulasekaran@users.noreply.github.com>
2021-04-28 11:52:36 +0530

    Delete views.py

--- views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here. 	
-from django.shortcuts import render
-from .models import user_entries
-
-def home(request):
-	 all_users = user_entries.objects.all()
-	 return render(request, 'home.html', {'all':all_users})
